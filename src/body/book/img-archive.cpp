64506a6f5669272d9a415b9c5fd66759b6dd21ca
蔡心宇 <haocxy@qq.com>
2022-05-29 17:02:31 +0800

    a

---
+++ src/body/book/img-archive.cpp
@@ -0,0 +1,10 @@
+#include "img-archive.h"
+
+
+namespace myapp {
+
+void ImgArchive::load(const fs::path &archiveFile)
+{
+}
+
+}
--- src/body/book/img-archive.h
+++ src/body/book/img-archive.h
@@ -1,11 +1,17 @@
 #pragma once
 
+#include "util/fs.h"
+
 #include "image.h"
 
 
 namespace myapp {
 
 class ImgArchive {
+public:
+    void load(const fs::path &archiveFile);
+
+private:
 
 };
 
