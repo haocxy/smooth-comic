340473f3099bda26dc740712725283e72b513232
蔡心宇 <haocxy@qq.com>
2022-06-08 00:27:18 +0800

    为thumb_cache表设置主键

--- src/body/book/sql/asset.thumb-img-table-create.sql
+++ src/body/book/sql/asset.thumb-img-table-create.sql
@@ -1,5 +1,6 @@
 create table if not exists thumb_cache (
     archive_path text not null,
     entry_path text not null,
-    img blob not null
+    img blob not null,
+    constraint c_pk primary key (archive_path, entry_path)
 );
