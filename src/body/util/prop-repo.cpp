4ab63c3682e7564d48658d86eeaa650526a90305
蔡心宇 <haocxy@qq.com>
2022-06-19 17:38:59 +0800

    add class PropRepo

---
+++ src/body/util/prop-repo.cpp
@@ -0,0 +1,121 @@
+#include "prop-repo.h"
+
+#include <format>
+
+
+namespace myapp
+{
+
+static std::string mkCreateTableSql(const std::string &repo)
+{
+    return std::format("create table if not exists {} (key text primary key, val not null);", repo);
+}
+
+static std::string mkSelectSql(const std::string &repo)
+{
+    return std::format("select val from {} where key = ?;", repo);
+}
+
+static std::string mkInsertOrReplaceSql(const std::string &repo)
+{
+    return std::format("insert or replace into {} values (?,?);", repo);
+}
+
+PropRepo::PropRepo(sqlite::Database &db, const std::string &repo)
+    : db_(db)
+    , repo_(repo)
+{
+    db_.exec(mkCreateTableSql(repo));
+
+    stmtSelect_.open(db_, mkSelectSql(repo));
+
+    stmtInsertOrReplace_.open(db_, mkInsertOrReplaceSql(repo));
+}
+
+PropRepo::~PropRepo()
+{
+}
+
+bool PropRepo::getValue(const u8str &key, u8str &val) const
+{
+    stmtSelect_.reset();
+    stmtSelect_.arg(key);
+    if (stmtSelect_.nextRow()) {
+        stmtSelect_.getValue(0, val);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void PropRepo::setValue(const u8str &key, const u8str &val)
+{
+    stmtInsertOrReplace_.reset();
+    stmtInsertOrReplace_.arg(key).arg(val);
+    stmtInsertOrReplace_.execute();
+}
+
+void PropRepo::set(const u8str &key, const u8str &val)
+{
+    setValue(key, val);
+}
+
+void PropRepo::set(const u8str &key, bool val)
+{
+    setValue(key, val ? 1 : 0);
+}
+
+void PropRepo::set(const u8str &key, int val)
+{
+    setValue(key, val);
+}
+
+void PropRepo::set(const u8str &key, long val)
+{
+    setValue(key, val);
+}
+
+void PropRepo::set(const u8str &key, long long val)
+{
+    setValue(key, val);
+}
+
+bool PropRepo::get(const u8str &key, bool &to) const
+{
+    int n = 0;
+    if (getValue(key, n)) {
+        to = (n != 0);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool PropRepo::get(const u8str &key, int &to) const
+{
+    return getValue(key, to);
+}
+
+bool PropRepo::get(const u8str &key, long &to) const
+{
+    return getValue(key, to);
+}
+
+bool PropRepo::get(const u8str &key, long long &to) const
+{
+    return getValue(key, to);
+}
+
+bool PropRepo::get(const u8str &key, u8str &to) const
+{
+    return getValue(key, to);
+}
+
+bool PropRepo::get(const u8str &key, u32str &to) const
+{
+    return getValue(key, to);
+}
+
+
+
+}
---
+++ src/body/util/prop-repo.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include "core/fs.h"
+#include "core/basetype.h"
+
+#include "sqlite.h"
+
+
+namespace myapp
+{
+
+// 属性仓库
+// 为了提升性能和简化管理，把属性数据库中的某些相关性较强的属性放在一起
+// 底层对应一个数据表
+class PropRepo {
+public:
+
+    // db: 在哪个属性数据库中
+    // name: 属性仓库的名称，在底层对应数据表的名称
+    //
+    // 构造时会调用 loadFromDb 加载数据
+    PropRepo(sqlite::Database &db, const std::string &repo);
+
+    // 析构时会调用 saveToDb 存储数据
+    ~PropRepo();
+
+    void set(const u8str &key, const u8str &val);
+
+    void set(const u8str &key, bool val);
+
+    void set(const u8str &key, int val);
+
+    void set(const u8str &key, long val);
+
+    void set(const u8str &key, long long val);
+
+    bool get(const u8str &key, bool &to) const;
+
+    bool get(const u8str &key, int &to) const;
+
+    bool get(const u8str &key, long &to) const;
+
+    bool get(const u8str &key, long long &to) const;
+
+    bool get(const u8str &key, u8str &to) const;
+
+    bool get(const u8str &key, u32str &to) const;
+
+private:
+    void setValue(const u8str &key, const u8str &val);
+
+    bool getValue(const u8str &key, u8str &val) const;
+
+    template <typename T>
+    bool getValue(const u8str &key, T &val) const {
+        u8str u8val;
+        if (getValue(key, u8val)) {
+            val = static_cast<T>(u8val);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+private:
+    sqlite::Database &db_;
+    const std::string repo_;
+    mutable sqlite::Statement stmtSelect_;
+    mutable sqlite::Statement stmtInsertOrReplace_;
+};
+
+}
