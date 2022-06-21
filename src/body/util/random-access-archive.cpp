d8f844d4a4937bcac16a14bb1fbb61f7026dd834
蔡心宇 <haocxy@qq.com>
2022-06-22 01:19:43 +0800

    add class RandomAccessArchive，未测试

---
+++ src/body/util/random-access-archive.cpp
@@ -0,0 +1,76 @@
+#include "random-access-archive.h"
+
+
+namespace myapp {
+
+static i32 getEntryCount(const fs::path &file)
+{
+    wrapper::libarchive::Archive archive(file);
+
+    i32 count = 0;
+
+    while (archive.nextEntry()) {
+        ++count;
+        archive.skipContent();
+    }
+
+    return count;
+}
+
+void RandomAccessArchive::open(const fs::path &file)
+{
+    close();
+
+    file_ = file;
+
+    entryCount_ = getEntryCount(file);
+
+    archive_ = std::make_unique<wrapper::libarchive::Archive>(file_);
+}
+
+void RandomAccessArchive::close()
+{
+    curEntry_ = 0;
+    archive_ = nullptr;
+    entryCount_ = 0;
+    file_.clear();
+}
+
+RandomAccessArchive::Entry RandomAccessArchive::entryAt(i32 index)
+{
+    checkIndex(index);
+
+    seekCurEntryTo(index);
+
+    if (archive_->nextEntry()) {
+        Entry entry;
+        entry.name = archive_->path();
+        entry.data = archive_->readContent();
+
+        ++curEntry_;
+
+        return entry;
+    } else {
+        throw std::runtime_error(std::format("RandomAccessArchive::entryDataAt({}) failed", index));
+    }
+}
+
+void RandomAccessArchive::seekCurEntryTo(i32 index)
+{
+    checkIndex(index);
+
+    if (index < curEntry_) {
+        archive_ = std::make_unique<wrapper::libarchive::Archive>(file_);
+        curEntry_ = 0;
+    }
+
+    for (; curEntry_ < index; ++curEntry_) {
+        if (archive_->nextEntry()) {
+            archive_->skipContent();
+        } else {
+            throw std::runtime_error(std::format("RandomAccessArchive::seekCurEntryTo({}) failed, curEntry_ = {}", index, curEntry_));
+        }
+    }
+}
+
+}
---
+++ src/body/util/random-access-archive.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "core/basetype.h"
+
+#include "libarchive-wrapper.h"
+
+namespace myapp {
+
+class RandomAccessArchive {
+public:
+    class Entry {
+    public:
+        u32str name;
+        Buff data;
+    };
+
+    RandomAccessArchive() {}
+
+    RandomAccessArchive(const fs::path &file) {
+        open(file);
+    }
+
+    ~RandomAccessArchive() {
+        close();
+    }
+
+    void open(const fs::path &file);
+
+    void close();
+
+    i32 entryCount() const {
+        return entryCount_;
+    }
+
+    Entry entryAt(i32 index);
+
+private:
+    void seekCurEntryTo(i32 index);
+
+    void checkIndex(i32 index) const {
+        if (0 <= index && index < entryCount_) {
+            return;
+        }
+
+        throw std::out_of_range(std::format("RandomAccessArchive::checkIndex({})", index));
+    }
+
+private:
+    fs::path file_;
+    i32 entryCount_{};
+    mutable uptr<wrapper::libarchive::Archive> archive_;
+    mutable i32 curEntry_{};
+};
+
+}
