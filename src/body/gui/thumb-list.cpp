d7d35b633b5481e7d21eb8e879b3787e21a0060d
蔡心宇 <haocxy@qq.com>
2022-06-22 15:42:45 +0800

    add class ThumbList

---
+++ src/body/gui/thumb-list.cpp
@@ -0,0 +1,15 @@
+#include "thumb-list.h"
+
+
+namespace myapp {
+
+ThumbList::ThumbList(QWidget *parent)
+    : QWidget(parent)
+{
+}
+
+ThumbList::~ThumbList()
+{
+}
+
+}
---
+++ src/body/gui/thumb-list.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <QWidget>
+
+
+namespace myapp {
+
+class ThumbList : public QWidget {
+    Q_OBJECT
+public:
+    ThumbList(QWidget *parent = nullptr);
+
+    virtual ~ThumbList();
+
+private:
+
+};
+
+}
