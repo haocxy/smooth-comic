ba8d3e21e7664f7bac69dccd525770c91d2cdaf4
蔡心宇 <haocxy@qq.com>
2022-06-26 23:24:10 +0800

    初步添加保持比例自动适应显示区域的功能

--- src/body/gui/page-widget.cpp
+++ src/body/gui/page-widget.cpp
@@ -1,6 +1,7 @@
 #include "page-widget.h"
 
 #include <QPainter>
+#include <QStyle>
 
 
 namespace myapp {
@@ -17,12 +18,32 @@ PageWidget::~PageWidget()
 {
 }
 
+static QSize calcShowSize(const QSize &imgSize, const QSize &areaSize)
+{
+    if (imgSize.width() > 0 && imgSize.height() > 0) {
+        const float scaleW = float(areaSize.width()) / imgSize.width();
+        const float scaleH = float(areaSize.height()) / imgSize.height();
+        const float scale = std::min(scaleW, scaleH);
+        return imgSize * scale;
+    } else {
+        return QSize();
+    }
+}
+
 void PageWidget::paintEvent(QPaintEvent *e)
 {
     QPainter p(this);
+    p.fillRect(rect(), palette().color(QPalette::ColorRole::AlternateBase));
 
-    p.fillRect(rect(), Qt::gray);
-    p.drawPixmap(rect(), img_);
+    const QSize areaSize = size();
+    const QSize showSize = calcShowSize(img_.size(), areaSize);
+
+    if (showSize.isValid()) {
+        const auto x = (areaSize.width() - showSize.width()) / 2;
+        const auto y = (areaSize.height() - showSize.height()) / 2;
+        const QRect showRect = QRect(QPoint(x, y), showSize);
+        p.drawPixmap(showRect, img_);
+    }
 }
 
 }
