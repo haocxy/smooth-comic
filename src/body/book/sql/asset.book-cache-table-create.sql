74126b494a51dcf2f4424dc5619281075f92e2c0
蔡心宇 <haocxy@qq.com>
2022-07-03 17:22:34 +0800

    删除无用文件

--- src/body/book/sql/asset.book-cache-table-create.sql
+++
@@ -1,6 +0,0 @@
-create table if not exists book_cache (
-    entry_path text not null,
-    width int not null,
-    height int not null,
-    constraint c_pk primary key (entry_path)
-);
--- src/body/book/sql/asset.thumb-cache-table-create.sql
+++
@@ -1,5 +0,0 @@
-create table if not exists thumb_cache (
-    entry_path text not null,
-    img blob not null,
-    constraint c_pk primary key (entry_path)
-);
