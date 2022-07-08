3ee00eda465b5db229a5ba65120f2e5e64f998a8
蔡心宇 <haocxy@qq.com>
2022-07-08 14:20:40 +0800

    comment

--- src/gui/controller/controller.h
+++ src/gui/controller/controller.h
@@ -9,6 +9,13 @@ namespace myapp {
 
 class Book;
 
+// 控制器
+//
+// 有很多功能都能够从多个地方触发，这样可以避免不同模块重复实现相同的功能，
+// 也可以保证界面变化一致且易于维护
+// 
+// 所有 非展示性 的 GUI 操作（离如翻页、打开文件等）的控制中心
+// 不要在 非展示性 的 GUI 模块间直接互相使用功能，而是通过控制器解除不同模块的耦合
 class Controller : public QObject {
     Q_OBJECT
 public:
