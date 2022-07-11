e07f3032b4886bd636976dbe68c1956237ff4614
蔡心宇 <haocxy@qq.com>
2022-07-11 22:12:18 +0800

    object name

--- src/gui/controll-bar-area/scale-setting-popup.cpp
+++ src/gui/controll-bar-area/scale-setting-popup.cpp
@@ -9,6 +9,8 @@ namespace myapp {
 ScaleSettingPopup::ScaleSettingPopup(QWidget *parent)
     : PopupWidget(parent)
 {
+    setObjectName("ScaleSettingPopup");
+
     ui_ = new Ui::ScaleSetting;
     ui_->setupUi(this);
 }
