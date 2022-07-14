01ad259314a4832beb8b13e9f0b9cc9ae6f91527
蔡心宇 <haocxy@qq.com>
2022-07-14 20:09:35 +0800

    添加Qt翻译系统和Qt资源系统支持

---
+++ trans/trans.zh_CN.ts
@@ -0,0 +1,144 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>BookStatus</name>
+    <message>
+        <location filename="../src/gui/book-area/status-area/book-status.ui" line="14"/>
+        <location filename="../src/gui/book-area/status-area/gen.book-status.ui.h" line="85"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/book-area/status-area/book-status.ui" line="20"/>
+        <location filename="../src/gui/book-area/status-area/gen.book-status.ui.h" line="86"/>
+        <source>current page:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/book-area/status-area/book-status.ui" line="27"/>
+        <location filename="../src/gui/book-area/status-area/book-status.ui" line="48"/>
+        <location filename="../src/gui/book-area/status-area/gen.book-status.ui.h" line="87"/>
+        <location filename="../src/gui/book-area/status-area/gen.book-status.ui.h" line="89"/>
+        <source>0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/book-area/status-area/book-status.ui" line="41"/>
+        <location filename="../src/gui/book-area/status-area/gen.book-status.ui.h" line="88"/>
+        <source>total page:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ScaleSettingPopup</name>
+    <message>
+        <location filename="../src/gui/controll-bar-area/scale-setting.ui" line="14"/>
+        <location filename="../src/gui/controll-bar-area/gen.scale-setting.ui.h" line="128"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/scale-setting.ui" line="44"/>
+        <location filename="../src/gui/controll-bar-area/gen.scale-setting.ui.h" line="129"/>
+        <source>No Scale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/scale-setting.ui" line="54"/>
+        <location filename="../src/gui/controll-bar-area/gen.scale-setting.ui.h" line="130"/>
+        <source>By Size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/scale-setting.ui" line="61"/>
+        <location filename="../src/gui/controll-bar-area/gen.scale-setting.ui.h" line="131"/>
+        <source>By Width</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/scale-setting.ui" line="68"/>
+        <location filename="../src/gui/controll-bar-area/gen.scale-setting.ui.h" line="132"/>
+        <source>By Height</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/scale-setting.ui" line="75"/>
+        <location filename="../src/gui/controll-bar-area/gen.scale-setting.ui.h" line="133"/>
+        <source>Manual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/scale-setting.ui" line="82"/>
+        <location filename="../src/gui/controll-bar-area/gen.scale-setting.ui.h" line="134"/>
+        <source>Manual &amp;&amp; Fix</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/scale-setting.ui" line="107"/>
+        <location filename="../src/gui/controll-bar-area/gen.scale-setting.ui.h" line="135"/>
+        <source>0%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/scale-setting.ui" line="136"/>
+        <location filename="../src/gui/controll-bar-area/gen.scale-setting.ui.h" line="136"/>
+        <source>200%</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>myapp::AppWidget</name>
+    <message>
+        <location filename="../src/gui/app-widget.cpp" line="112"/>
+        <source>Open Comic</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>myapp::BookStatus</name>
+    <message>
+        <location filename="../src/gui/book-area/status-area/book-status.cpp" line="96"/>
+        <source>Unsupported archive file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>myapp::ControllBarArea</name>
+    <message>
+        <location filename="../src/gui/controll-bar-area/controll-bar-area.cpp" line="48"/>
+        <source>Thumb Area</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/controll-bar-area.cpp" line="52"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/controll-bar-area.cpp" line="56"/>
+        <source>Rotate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/controll-bar-area.cpp" line="60"/>
+        <source>Left</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/controll-bar-area.cpp" line="65"/>
+        <source>Right</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/controll-bar-area.cpp" line="70"/>
+        <source>Scale</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/gui/controll-bar-area/controll-bar-area.cpp" line="75"/>
+        <source>Menu</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
