308d3df517d9288b3de75e606b7447be32a5355c
蔡心宇 <haocxy@qq.com>
2022-07-23 19:40:48 +0800

    a

--- smooth-comic-android/src/com/smoothapplication/smoothcomic/MyAppActivity.java
+++ smooth-comic-android/src/com/smoothapplication/smoothcomic/MyAppActivity.java
@@ -1,4 +1,19 @@
 package com.smoothapplication.smoothcomic;
 
-public class MyAppActivity {
+import android.os.Bundle;
+import android.util.Log;
+
+import org.qtproject.qt.android.bindings.QtActivity;
+
+public class MyAppActivity extends QtActivity {
+
+    private final static String TAG = "smooth-comic";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "MyAppActivity::onCreate before QtActivity::onCreate");
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "MyAppActivity::onCreate after QtActivity::onCreate");
+    }
+
 }
